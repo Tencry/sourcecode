7c0ca29253e13c0bf021389286008f71be5e74e6
Arman [Tencry] Sarsenov <tencry@mail.ru>
2019-11-27 17:58:33 +0600

    Real calc of temperature

--- clang/kernighan/chap1/celsius.c
+++ clang/kernighan/chap1/celsius.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /* вывод таблицы температур по Фаренгейту и Цельсию
-     для fahr = 0, 20, ..., 300 */
+     для fahr = 0, 20, ..., 300
+     версия с вещественной арифметикой */
 main()
 {
-	int fahr, celsius;
+	float fahr, celsius;
 	int lower, upper, step;
 
 	lower = 0;	/* нижняя граница температур в таблице */
@@ -13,8 +14,8 @@ main()
 
 	fahr = lower;
 	while (fahr <= upper) {
-		celsius = 5 * (fahr - 32) / 9;
-		printf("%3d %6d\n", fahr, celsius);
+		celsius = (5.0/9.0) * (fahr - 32.0);
+		printf("%3.0f %6.1f\n", fahr, celsius);
 		fahr = fahr + step;
 	}
 }
