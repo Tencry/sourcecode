b22a2266e607a4b889f6b439bf18bc4c260d8dc3
Arman [Tencry] Sarsenov <tencry@mail.ru>
2019-11-27 17:48:40 +0600

    Calc temperature in Celsius.

---
+++ clang/kernighan/chap1/celsius.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/* вывод таблицы температур по Фаренгейту и Цельсию
+     для fahr = 0, 20, ..., 300 */
+main()
+{
+	int fahr, celsius;
+	int lower, upper, step;
+
+	lower = 0;	/* нижняя граница температур в таблице */
+	upper = 300;	/* верхняя граница */
+	step = 20;	/* величина шага */
+
+	fahr = lower;
+	while (fahr <= upper) {
+		celsius = 5 * (fahr - 32) / 9;
+		printf("%d\t%d\n", fahr, celsius);
+		fahr = fahr + step;
+	}
+}
