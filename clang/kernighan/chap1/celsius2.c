2dc77ca6ee56a0872d25ccc9e5bac2521cd8f1ee
Arman [Tencry] Sarsenov <tencry@mail.ru>
2019-12-05 13:35:33 +0600

    Calc Celsius ver.2

---
+++ clang/kernighan/chap1/celsius2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/* вывод таблицы температуры по Фаренгейту и Цельсию */
+main()
+{
+	int  fahr;
+
+	for (fahr = 0; fahr <= 300; fahr = fahr + 20)
+		printf("%3d %6.1f\n", fahr, (5./9.)*(fahr-32));
+}
