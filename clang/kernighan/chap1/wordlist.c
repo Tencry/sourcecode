1906884b97cdc984b1e9933acf336b0dfe38acea
Arman [Tencry] Sarsenov <tencry@mail.ru>
2019-12-06 16:10:56 +0600

    Wordlist ver.2

--- clang/kernighan/chap1/wordlist.c
+++ clang/kernighan/chap1/wordlist.c
@@ -1,12 +1,32 @@
 #include <stdio.h>
 
+#define IN	1
+#define OUT	0
+
 main()
 {
-	int c;
+	int c, state;
 
+	state = OUT;
 	while ((c = getchar()) != EOF) {
-		if (c == ' ' || c == '\t')
+		/*
+		if (c == ' ' || c == '\t' || c == '\n')
+			state = OUT;
+		else if (state == OUT) {
+			state = IN;
 			putchar('\n');
-		else putchar(c);
+		}
+		if (state == IN)
+			putchar(c);
+		*/
+
+		if (c != ' ' && c != '\t' && c != '\n') {
+			state = IN;
+			putchar(c);
+		}
+		else if (state == IN) {
+			state = OUT;
+			putchar('\n');
+		}
 	}
 }
