51f2dcb523d67c70ecbbd9588718a577c0544e85
Arman [Tencry] Sarsenov <tencry@mail.ru>
2019-12-11 15:05:35 +0600

    Reverse function.

---
+++ clang/kernighan/chap1/strings4.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#define MAXLINE 1000 /* максимальная длина строки в потоке */
+
+int getline(char line[], int maxline);
+void reverse(char line[], int length);
+
+/* вывод самой длинной строки в потоке */
+main()
+{
+	int len;		/* длина текущей строки */
+	char line[MAXLINE];	/* текущая введенная строка */
+
+	while ((len = getline(line, MAXLINE)) > 0) {
+		reverse(line, len);
+		printf("%s", line);
+	}
+
+	return 0;
+}
+
+/* getline: считывает строку в s, возвращает ее длину */
+int getline(char s[], int lim)
+{
+	int c, i;
+
+	for (i = 0; i<lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
+		s[i] = c;
+	if (c == '\n') {
+		s[i] = c;
+		++i;
+	}
+	s[i] = '\0';
+	return i;
+}
+
+void reverse(char line[], int length)
+{
+	int i;
+	char c;
+
+	i = 0;
+	while (i < length) {
+		c = line[i];
+		line[i] = line[--length];
+		line[length] = c;
+		++i;
+	}
+}
